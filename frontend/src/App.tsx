9be7a20b344d2bb7890438bc300f5c2f7f64fbd1
Yücel Çubuk <yucelcubuk851@gmail.com>
2025-02-28 10:55:44 +0300

    Port ekleme işlemi tamamlandı

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import PortList from "./components/PortList";
 import PortForm from "./components/PortForm";
+import { Dialog } from "primereact/dialog";
 import { Button } from "primereact/button";
-
 import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
+import { Port } from "./types/portTypes"; // Port arayüzünü ortak dosyadan alıyoruz
 
 
 
 const App: React.FC = () => {
-  const [ isDialogVisible, setDialogVisible ] = useState(false);
+  const [ports, setPorts] = useState<Port[]>([]);
+  const [isDialogVisible, setDialogVisible] = useState(false);
+
+  const addPort = (newPort: Omit<Port, "id">) => {
+    setPorts([...ports, { ...newPort, id: ports.length + 1 }]); // Yeni portu ekle
+    setDialogVisible(false); // Dialog'u kapat
+  };
+
   return (
     <div className="App">
-      <h1>Port Yönetim Uygulaması </h1>
-
-      {/* Butona basılınca açılacak */ }
-      <Button label='Yeni Port Ekle ' icon="pi pi-plıus"  onClick={() => setDialogVisible(true)}  />
-
-        {/* PortForm bileşenini açılır pencere içinde göstermek için */}
-      < PortForm visible={isDialogVisible} onHide={() => setDialogVisible(false)} />
-      
-      {/* mevcut port listesini göstermek için */}
-      <PortList /> 
+      <h1>Port Yönetim Uygulaması</h1>
+      <Button label="Yeni Port Ekle" icon="pi pi-plus" onClick={() => setDialogVisible(true)} />
+      <Dialog header="Port Ekle" visible={isDialogVisible} onHide={() => setDialogVisible(false)} style={{ width: "400px" }}>
+        < PortForm onAddPort={addPort} 
+  visible={isDialogVisible} 
+  onHide={() => setDialogVisible(false)} 
+   />
+      </Dialog>
+      <PortList ports={ports} />
     </div>
   );
 };
-
-
-
 
 export default App;
--- frontend/src/components/PortForm.tsx
+++ frontend/src/components/PortForm.tsx
@@ -1,68 +1,75 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import { Dialog } from "primereact/dialog";
 import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
+import { Port } from "../types/portTypes"; // Port arayüzünü ortak dosyadan alıyoruz
 
-const PortForm: React.FC<{visible: boolean; onHide: () => void  }> = ({ visible, onHide}) => { 
-    const initialValues = {
-        portNumber: "",
-        projectName: "",
-        applicationName: "",
-        description: "",
-    };
+// 📌 Props tanımlandı!
+interface PortFormProps {
+  onAddPort: (port: Omit<Port, "id">) => void;
+  visible: boolean;
+  onHide: () => void;
+}
 
-    const validationSchema = Yup.object({
-        portNumber: Yup.number()
-              .typeError("Port numarası sayı olmalıdır")
-              .required("Port numarası zorunludur"),
-              projectName: Yup.string().required("Uygulama adı zorunludur"),
-              description: Yup.string()
-    });
+const PortForm: React.FC<PortFormProps> = ({ onAddPort, visible, onHide }) => {
+  const initialValues = {
+    portNumber: "",
+    projectName: "",
+    applicationName: "",
+    description: "",
+  };
 
+  const validationSchema = Yup.object({
+    portNumber: Yup.string().required("Port numarası zorunludur"),
+    projectName: Yup.string().required("Proje adı zorunludur"),
+    applicationName: Yup.string().required("Uygulama adı zorunludur"),
+    description: Yup.string(),
+  });
 
-    const handleSubmit = (values: typeof initialValues) =>{
-        console.log("Form verileri: ", values);
-        onHide();
-    };
+  return (
+    <Dialog header="Port Ekle" visible={visible} onHide={onHide} style={{ width: "400px" }}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values, { resetForm }) => {
+          onAddPort(values); // 📌 Form verisini App.tsx'e gönderecek!
+          resetForm(); // Formu temizle
+          onHide(); // Formu kapat
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div>
+              <label>Port Numarası:</label>
+              <Field type="text" name="portNumber" className="p-inputtext" />
+              <ErrorMessage name="portNumber" component="div" className="p-error" />
+            </div>
 
+            <div>
+              <label>Proje Adı:</label>
+              <Field type="text" name="projectName" className="p-inputtext" />
+              <ErrorMessage name="projectName" component="div" className="p-error" />
+            </div>
 
-    return (
-        <Dialog header="Port Ekle" visible={visible} onHide={onHide} style= {{width: "400px" }}>
-            <Formik initialValues={initialValues} validationSchema={validationSchema}  onSubmit={handleSubmit}>
-                { ( { isSubmitting }) => (
-                    <Form>
-                        <div className="p-field">
-                            <label>Port Numarası: </label>
-                            <Field type="text"  name="portNumber" className="p-inputtext" />
-                            <ErrorMessage name="portNumber"  component="div" className="p-error" />
+            <div>
+              <label>Uygulama Adı:</label>
+              <Field type="text" name="applicationName" className="p-inputtext" />
+              <ErrorMessage name="applicationName" component="div" className="p-error" />
+            </div>
 
-                        </div>
-                        <div className="p-field">
-                            <label>Proje adı:</label>
-                            <Field type="text" name="projectName" className="p-inputtext" />
-                            <ErrorMessage name="projectName" component="div" className="p-error" />  
-                        </div>
+            <div>
+              <label>Açıklama:</label>
+              <Field as="textarea" name="description" className="p-inputtext" />
+              <ErrorMessage name="description" component="div" className="p-error" />
+            </div>
 
-                        <div className="p-field">
-                        <label>Uygulama Adı:</label>
-                        <Field type="text" name="applicationName" className="p-inputtext" />
-                        <ErrorMessage name="applicationName" component="div" className="p-error" />
-                        </div> 
-
-                        <div className="p-field">
-                            <label>Açıklama:</label>
-                            <Field as="textarea" name="description" className="p-inputtext" />
-                            <ErrorMessage name="description" component="div" className="p-error" />
-                         </div>
-                        <Button type="submit" label="Ekle" className="p-button-primary" disabled={isSubmitting} />
-
-                    </Form>
-                )}
-            </Formik>
-        </Dialog>
-        
-    );
+            <Button type="submit" label="Ekle" className="p-button-primary" disabled={isSubmitting} />
+          </Form>
+        )}
+      </Formik>
+    </Dialog>
+  );
 };
 
 export default PortForm;
